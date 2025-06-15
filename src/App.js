cc92f65916e270eba355049f2363002f9eb88a09
Ankur <ankursharma7697@gmail.com>
2025-06-15 21:34:52 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.get("/user", (req, res)=>{
+    res.send({firstname: "Ankur" , lastname:"Sharma"});
+})
+
+app.post("/user", (req, res)=>{
+    res.send("Data sent")
+})
+
 app.use("/test",(req, res) =>{
-    res.send("server is listening Ankur")
+    res.send("server is listening Ankur");
     
-})
-app.use("/login", (req,res)=>{
-    res.send("Server is not listening ")
-})
+});
+
 
 app.listen(9000, ()=>{
     console.log("Server is Listening")
